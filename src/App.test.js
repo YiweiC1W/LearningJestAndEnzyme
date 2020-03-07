56e24d1b05c9bab9b35f8a01b61f85e795e21194
Yiwei Chen <kanyeishere@gmail.com>
2020-03-08 10:45:50 +1100

    learning jest and enzyme to test a react application

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,70 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapater from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapater() });
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+test("renders without error", () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders increment button", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
+});
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
+test("counter starts at 0", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
+test("clicking button increments counter display", () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter: counter });
+
+  const button = findByTestAttr(wrapper, "increment-button");
+  button.simulate("click");
+
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
+test("clicking button decrements counter display", () => {
+    const counter = 7;
+    const wrapper = setup(null, { counter: counter });
+
+    const button = findByTestAttr(wrapper, "decrease-button");
+    button.simulate("click");
+
+    const counterDisplay = findByTestAttr(wrapper, "counter-display");
+    expect(counterDisplay.text()).toContain(counter - 1);
+});
+test("clicking button decrements no less than zero", () => {
+    const counter = 0;
+    const wrapper = setup(null, { counter: counter });
+
+    const button = findByTestAttr(wrapper, "decrease-button");
+    button.simulate("click");
+    button.simulate("click");
+    button.simulate("click");
+    const counterDisplay = findByTestAttr(wrapper, "counter-display");
+    expect(counterDisplay.text()).toContain(0);
 });
